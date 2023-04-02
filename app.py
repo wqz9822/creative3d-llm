ca439f745350928e7334e94e53d6ae140ce38047
George Wang <wqz9822@gmail.com>
2023-04-02 13:18:07 -0400

    update flask server

--- app.py
+++ app.py
@@ -1,14 +1,35 @@
 from flask import Flask, request, render_template
+from openai_utils import handle_message
+from prompt_maker import MuseumPrompt
+from text_to_speech import read_text
+
+from threading import Thread
 
 app = Flask(__name__)
 
-current_location = None 
+current_author = None 
+current_question = None
+audio_thread = None
 
-@app.route('/update_location')
-def data():
+def read_response(response):
+    print("Response:", response) 
+    read_text(response, "") 
+
+@app.route('/update_author')
+def update_author():
     # here we want to get the value of user (i.e. ?user=some-value)
-    current_location = request.args.get('location')
-    return f"Current location is: {current_location}"
+    current_author = request.args.get('author')
+    return f"Current author is: {current_author}"
+
+@app.route('/ask_question')
+def ask_question():
+    current_question = request.args.get('question')
+    prompt = MuseumPrompt.get_prompt(current_question)
+    print("Prompt:", prompt) 
+    response = handle_message(prompt, "") 
+    audio_thread = Thread(target=read_response, args=(response,)) 
+    audio_thread.start()
+    return f"Q: {current_question} A: {response}"
 
 @app.route('/')
 def index():
@@ -16,3 +37,4 @@ def index():
 
 if __name__ == '__main__':
     app.run()
+    
