c35a6eb5c9431bf93207e0447729273563896e5b
George Wang <wqz9822@gmail.com>
2023-04-02 00:07:18 -0400

    add input loop; refactor

--- main.py
+++ main.py
@@ -5,30 +5,49 @@ from openai_utils import handle_message
 from speech_to_text import record_audio, transcribe_audio
 from text_to_speech import read_text, split_paragraph_into_sentences, group_sentences
 
+def transcribe_and_receive_response():
+    with tempfile.NamedTemporaryFile(suffix=".wav", delete=False) as temp_audio_file:
+        record_audio(temp_audio_file.name)
+        transcript = transcribe_audio(temp_audio_file.name)
+        print("Transcript:", transcript)
+        return handle_message(transcript["text"], args.openai_api_key) 
+
+def read_response(response, elevenlabs_api_key): 
+    if not args.elevenlabs_api_key:
+        print("Skipping ElevenLabs API call as no API key was provided.") 
+        return
+    # groups = group_sentences(split_paragraph_into_sentences(response), 5) 
+    # for group in groups: 
+    #     if group: 
+    #         text=" ".join(group)
+    #         read_text(text, args.elevenlabs_api_key, streaming=True)
+    read_text(response, args.elevenlabs_api_key, streaming=True)
+
 # Define main entry point
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Chat with GPT using OpenAI')
     parser.add_argument('--openai_api_key', type=str, required=True, help='the OpenAI API key to use')
     parser.add_argument('--elevenlabs_api_key',type=str, required=False, help='the ElevenLabs API key to use')
     args = parser.parse_args()
-
     # Set up OpenAI API key
     openai.api_key = args.openai_api_key
 
-    with tempfile.NamedTemporaryFile(suffix=".wav", delete=False) as temp_audio_file:
-        record_audio(temp_audio_file.name)
-        transcript = transcribe_audio(temp_audio_file.name)
-        print("Transcript:", transcript)
-        response = handle_message(transcript["text"], args.openai_api_key) 
-        print("Response:", response) 
-        if args.elevenlabs_api_key:
-            # groups = group_sentences(split_paragraph_into_sentences(response), 5) 
-            # for group in groups: 
-            #     if group: 
-            #         text=" ".join(group)
-            #         read_text(text, args.elevenlabs_api_key, streaming=True)
-            read_text(response, args.elevenlabs_api_key, streaming=True)
+    while True:
+        user_input = input("Press 'm' to send a message, 'r' to record audio, or 'e' to exit: ")
+        if user_input == 'm':
+            prompt = input("Enter your message: ")
+            response = handle_message(prompt, args.openai_api_key) 
+            print("Response:", response) 
+            read_response(response, args.elevenlabs_api_key) 
+        elif user_input == 'r':
+            response = transcribe_and_receive_response()
+            print("Response:", response) 
+            read_response(response, args.elevenlabs_api_key) 
+        elif user_input == 'e':
+            break
         else:
-            print("Skipping ElevenLabs API call as no API key was provided.") 
+            print("Invalid input, please try again.")
+
+
 
     
