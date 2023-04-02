1b2e13b87c49493c2710f2884639069ed4e02f86
George Wang <wqz9822@gmail.com>
2023-04-02 13:45:37 -0400

    add author name

--- app.py
+++ app.py
@@ -1,32 +1,32 @@
 from flask import Flask, request, render_template
 from openai_utils import handle_message
-from prompt_maker import MuseumPrompt
+from prompt_maker import MuseumPrompt, AuthorPrompt
 from text_to_speech import read_text
 
 from threading import Thread
+import urllib.parse
+import os
 
 app = Flask(__name__)
 
+OPENAI_KEY =os.environ["OPENAI_KEY"] 
+ELEVEN_KEY = os.environ["ELEVEN_KEY"]
 current_author = None 
 current_question = None
 audio_thread = None
 
 def read_response(response):
     print("Response:", response) 
-    read_text(response, "") 
-
-@app.route('/update_author')
-def update_author():
-    # here we want to get the value of user (i.e. ?user=some-value)
-    current_author = request.args.get('author')
-    return f"Current author is: {current_author}"
+    read_text(response, ELEVEN_KEY) 
 
 @app.route('/ask_question')
 def ask_question():
     current_question = request.args.get('question')
-    prompt = MuseumPrompt.get_prompt(current_question)
+    current_author = urllib.parse.unquote(request.args.get('author_name')).replace('"', '')
+    print(current_author) 
+    prompt = AuthorPrompt.get_prompt(current_question, author_name=current_author)
     print("Prompt:", prompt) 
-    response = handle_message(prompt, "") 
+    response = handle_message(prompt, OPENAI_KEY) 
     audio_thread = Thread(target=read_response, args=(response,)) 
     audio_thread.start()
     return f"Q: {current_question} A: {response}"
